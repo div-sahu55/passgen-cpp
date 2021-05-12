d799a2a6f44b946b394ed71966a6dc314ac97aa2
Divyansh sahu <divyanshsahu80@gmail.com>
2021-05-12 14:27:57 +0530

    first attempt at passgen

--- src/engine.h
+++ src/engine.h
@@ -1,4 +1,24 @@
 // Define this class here.
+#include <iostream>
+#include <cstring>
 class RandomEngine {
 
+    private: 
+        uint8_t length = 0;
+        bool symbl = false;
+
+    public:
+        std::string password = "";
+    // Constructors:
+        RandomEngine(); //default constructor
+        explicit RandomEngine(uint8_t l); // To set the generator for the specific length
+        explicit RandomEngine(bool s); // To turn on symbol character set. Length remains default.
+        RandomEngine(uint8_t l, bool s); // To set length and symbol character set.
+
+    // Methods:
+        void setLength(uint8_t); // Set the length
+        void setSymbolSet(bool); // Turn on symbols
+        std::string getString(); // Returns a random string with configured options
+        uint8_t getLength();  // Return length of the generator
+        bool getSymbolSetStatus();  // Returns true if symbols are on
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <random>
 #include "version.h"
 #include "engine.h"
 void print_usage() {
-    // std::cout << "Usage:\n";
+    std::cout << "Usage:\n";
     std::cout << "passgen -L <length>       generates a random password with default character set of given length\n";
 	std::cout << "Example: passgen -L 20    generates a random password with default character set of length 20\n";
 	std::cout << "passgen -S                generates a random password with all character set of default length 8\n";
@@ -22,10 +24,10 @@ int main(int argc, char *argv[]){
         return 1;
     }
     // set the length of the character set to default, which is 8.
-    uint8_t length=8;
-    
+    int length=8;
     // set symbol set
-    bool symbol_set = false;
+    bool symbol_set = false,gen_pass=false;
+    RandomEngine r1((uint8_t)8,symbol_set);
     for(int i = 1; i<argc; i++) {
         if(strcmp(argv[i], "-S") == 0) {
             if(!symbol_set) {
@@ -50,7 +52,7 @@ int main(int argc, char *argv[]){
                 return 2;
             }
             if(pos != strlen(argv[i+1])) {
-                // This is a rare case when a string like "45jkl" is parsed to 45 and not bad input
+             // This is a rare case when a string like "45jkl" is parsed to 45 and not bad input
                 std::cerr << "Bad argument for password length\n";
                 return 2;
             }
@@ -58,8 +60,8 @@ int main(int argc, char *argv[]){
                 std::cerr << "password length must be in the range [1,128]\n";
                 return 2;
             }
-
             // If all goes right, we have a valid length
+            gen_pass=true;
             break;
         } else if (strcmp(argv[i], "-V") == 0){
             std::cout << "passgen version: " << passgen_VERSION_MAJOR << "." << passgen_VERSION_MINOR  << "."<< passgen_VERSION_REVISION << "\n";
@@ -73,6 +75,41 @@ int main(int argc, char *argv[]){
             return 2;
         }
     }
+    if(gen_pass || length==8){
+    //Random no. generaor:
+    char seq[100];
+    std::ifstream file("/dev/urandom", std::ios::in | std::ios::binary);
+    if(!file) {
+        std::cout << "Error opening file\n";
+    }
+    file.read(seq, sizeof(char)*100);
+    file.close();
     
+    std::seed_seq seed(seq, seq+100);
+    std::mt19937_64 gen(seed);
+    std::uniform_int_distribution<int64_t> dist(0, 100000);
+    //password generation:
+        r1.setLength(length);
+        r1.setSymbolSet(symbol_set);
+        std::string alpha_num="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        std::string symbols="!@#$%^&*-_";
+        std::string s;
+        std::string alpha_num_sym;
+        int mod=62;
+        if(r1.getSymbolSetStatus()==true){
+            alpha_num_sym= alpha_num + symbols;
+            mod=72;
+        }
+        else
+        alpha_num_sym= alpha_num;
+            for(int i=0;i<r1.getLength();i++){
+                int ind=dist(gen) % mod;
+                char c;
+                c= alpha_num_sym[ind];
+                s+=c;
+            }
+            r1.password=s;
+            std::cout<<r1.getString()<<std::endl;
+    }
     return 0;
 }
